1d9f2f1e5a2ae0c7b52e69c390ca7f1b61c557e5
margarida <>
2024-12-05 10:07:50 +0100

    Toucher Main Renome Fichier

--- Assets/ProjetTempoMGSC/Scripts/PlaySound.cs
+++ Assets/ProjetTempoMGSC/Scripts/PlaySound.cs
@@ -1,21 +1,22 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class PlaySound : MonoBehaviour, IPointerClickHandler
 {
     public float speed = 1.0f;
-
     public AudioClip clip;
+
     private static AudioSource audioSource;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        //point at camera
+        // point at camera
         //transform.LookAt(Camera.main.transform);
 
-        //find the AudioSource component on the "AudioSource" game object
-        if(audioSource == null)
+        // find the AudioSource component on the "AudioSource" game object
+        if (audioSource == null)
         {
             audioSource = GameObject.Find("AudioSource").GetComponent<AudioSource>();
         }
@@ -24,27 +25,32 @@ public class PlaySound : MonoBehaviour, IPointerClickHandler
     // Update is called once per frame
     void Update()
     {
-        //déplacement de l'objet
+        // déplacement de l'objet
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
-
-        
     }
 
-    //Play a sound when the object is clicked
-
+    // Play a sound when the object is clicked
     public void OnPointerClick(PointerEventData eventData)
     {
         Debug.Log("OnPointerClick");
+        PlayTheSoundAndDestroy();
+    }
 
-        //if the distance between the object and camera is less than 1.5
-        if(Vector3.Distance(transform.position, Camera.main.transform.position) <1.5f)
+    public void OnHover(HoverEnterEventArgs args)
+    {
+        Debug.Log("OnHover");
+        PlayTheSoundAndDestroy();
+    }
+
+    public void PlayTheSoundAndDestroy()
+    {
+         // if the distance between the object and the camera is less than 1.5
+        if (Vector3.Distance(transform.position, Camera.main.transform.position) < 1.5f)
         {
             audioSource.PlayOneShot(clip);
 
-             //Destroy the object
-             Destroy(gameObject);
+            // Destroy the object
+            Destroy(gameObject);
         }
-
-
     }
 }
